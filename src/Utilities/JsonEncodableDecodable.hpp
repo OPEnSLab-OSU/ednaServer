91a972e91fbbdeae9ba3b7153897b263fdb11a60
kawinie <pechetrk@oregonstate.edu>
2020-03-13 00:01:42 -0700

    Update JsonEncodableDecodable.hpp
    + Change JsonObject to JsonVariant

--- src/Utilities/JsonEncodableDecodable.hpp
+++ src/Utilities/JsonEncodableDecodable.hpp
@@ -3,9 +3,9 @@
 #include <KPFoundation.hpp>
 #include <ArduinoJson.h>
 
-//
-// ─── SECTION  INTERFACE FOR CUSTOM JSON DECODING OBJECT ────────────────────────────
-//
+// ────────────────────────────────────────────────────────────────────────────────
+// ─── SECTION  INTERFACE FOR CUSTOM JSON DECODING OBJECT ─────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
 class JsonDecodable {
 public:
 	virtual const char * decoderName() const {
@@ -18,16 +18,16 @@ public:
 	}
 };
 
-//
-// ─── SECTION  INTERFACE FOR CUSTOM JSON ENCODING OBJECT ────────────────────────────
-//
+// ────────────────────────────────────────────────────────────────────────────────
+// ─── SECTION  INTERFACE FOR CUSTOM JSON ENCODING OBJECT ─────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
 class JsonEncodable {
 public:
 	virtual const char * encoderName() const {
 		return "Unnamed";
 	}
 
-	virtual bool encodeJSON(JsonObject & dest) const = 0;
+	virtual bool encodeJSON(JsonVariant & dest) const = 0;
 	virtual void save(const char * filepath) const {
 		raise(Error("JsonEncodable save needs override"));
 	}
