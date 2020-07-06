5afbe8a6228516cca3a6aa679009b162001b3b8e
Kawin <pechetrk@oregonstate.edu>
2020-07-06 14:05:17 +0700

    Added ScheduleReturnCode

---
+++ src/Application/ScheduleReturnCode.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+class ScheduleReturnCode {
+public:
+	enum Code { unavailable, operating, scheduled } _code;
+
+public:
+	ScheduleReturnCode(Code code) : _code(code) {}
+
+	Code code() const {
+		return _code;
+	}
+
+	const char * description() const {
+		switch (_code) {
+		case unavailable:
+			return "No active task";
+		case operating:
+			return "Task is being executed";
+		case scheduled:
+			return "Task scheduled for execution";
+		default:
+			raise("Unknown case: ScheduleReturnCode::description");
+			return "";
+		}
+	}
+
+	const char * c_str() const {
+		switch (_code) {
+		case unavailable:
+			return "unavailable";
+		case operating:
+			return "operating";
+		case scheduled:
+			return "scheduled";
+		default:
+			raise("Unknown case: ScheduleReturnCode::description");
+			return "";
+		}
+	}
+
+	// Implicit conversion to int
+	operator int() const {
+		return _code;
+	}
+};
