68597d1537e39483ba36ff1e207643d8c1245800
Kawin <pechetrk@oregonstate.edu>
2020-07-08 16:28:20 +0700

    Fixed logic in FileLoader

--- src/Utilities/FileLoader.hpp
+++ src/Utilities/FileLoader.hpp
@@ -6,21 +6,20 @@ class FileLoader {
 public:
 	bool createDirectoryIfNeeded(const char * dir) {
 		File folder = SD.open(dir, FILE_READ);
-		if (folder && !folder.isDirectory()) {
-			raise(Error("FileLoader: path already exists but not directory please remove the file"));
+		if (folder) {
+			if (folder.isDirectory()) {
+				folder.close();
+				return true;
+			}
+
+			raise(Error("FileLoader: path already exists but "
+						"not directory please remove the file"));
 		}
 
-		if (folder && folder.isDirectory()) {
-			return true;
-		}
-
-		if (!folder) {
-			bool success = SD.mkdir(dir);
-			print("FileLoader: creating directory...");
-			println(success ? "success" : "failed");
-			return success;
-		} else {
-			return true;
-		}
+		// folder doesn't exist
+		print("FileLoader: directory doesn't exist. Creating...");
+		bool success = SD.mkdir(dir);
+		println(success ? "success" : "failed");
+		return success;
 	}
 };
