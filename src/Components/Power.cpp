e95eef20962d03fccfe4cebe2607ec948007aa59
Nathan Jesudason <nathan.jesudason@gmail.com>
2021-07-11 13:25:30 -0700

    Change button to active low

--- src/Components/Power.cpp
+++ src/Components/Power.cpp
@@ -4,11 +4,11 @@ volatile unsigned long rtcInterruptStart = 0;
 volatile bool alarmTriggered             = false;
 
 void rtc_isr() {
-    if (((unsigned long) (millis() - rtcInterruptStart) < 1000) && ((unsigned long) (millis() - buttonInterruptStart) < 200)){
+    if (((unsigned long) (millis() - rtcInterruptStart) < 1000) && ((unsigned long) (millis() - buttonInterruptStart) < 1000)){
         return;
     }
 	
-	if(digitalRead(HardwarePins::BUTTON_PIN){
+	if(digitalRead(HardwarePins::BUTTON_PIN) == LOW){
 		buttonTriggered    = true;
     	buttonInterruptStart = millis();
     	buttonFlag = 1;
